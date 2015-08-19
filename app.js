58aa48a371b7966891a97ac6091eec89469927a1
urielhdz <uriel.hdzc@gmail.com>
2015-08-19 14:32:33 -0500

    Resueltos conflictos con el merge, se cambiaron los assets a la carpeta /public

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var cloudinary = require("cloudinary");
 var method_override = require("method-override");
-var app_password = "12345678";
-var Schema = mongoose.Schema;
 
-cloudinary.config({
-	cloud_name: "codigofacilito",
-	api_key: "692877912266946",
-	api_secret: "v0J8Ree2n0af_zIUcpE23Rh8eD8"
-});
+var models = require("./models.js");
 
+// cloudinary.config({
+// 	cloud_name: "codigofacilito",
+// 	api_key: "",
+// 	api_secret: ""
+// });
 var app = express();
-
+app.use(express.static('public'));
 app.get("/",function(req,res){
 	res.sendFile(__dirname+"/index.html");
 });
