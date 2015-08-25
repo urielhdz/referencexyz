7694dc98402dfa6d395cea2a17f578acd87f5102
urielhdz <uriel.hdzc@gmail.com>
2015-08-25 13:15:55 -0500

    Agregar variable articles

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cloudinary = require("cloudinary");
 var method_override = require("method-override");
 var ejs_layout_engine = require("ejs-mate")
 var mongoose = require("mongoose");
+var http = require("http");
 mongoose.connect("mongodb://localhost/referencexyz_test");
 var models = require("./models.js"),
 	Language = models.Language,
@@ -83,11 +84,12 @@ router.route("/lenguajes/:id")
 		if(req.params.id === "new"){
 			res.render("languages/new");	
 		}else{
+
 			Language.findById(req.params.id,function(err,language){
-				properties = Property.find({language: language._id},function(err,propiedades){
+				Property.find({language: language._id},function(err,propiedades){
+					console.log("\n\n\n\n"+propiedades+"\n\n\n\n");
 					res.render("languages/show",{language: language, propiedades: propiedades});
 				});
-				
 			});
 		}
 	})
@@ -138,7 +140,6 @@ router.route("/propiedades")
 
 router.route("/propiedades/:id")
 	.get(function(req,res){
-		console.log("\n\n\n\n"+req.params.id+"\n\n\n\n");
 		if(req.params.id == "new" || typeof req.params.id == "undefined"){
 			Language.find({},function(err,languages){
 				res.render("properties/new",{languages: languages});	
@@ -175,7 +176,41 @@ app.get("propiedades/:id/edit",function(req,res){
 app.get("/",function(req,res){
 	Property.count({},function(err,total_propiedades){
 		if(err){console.log(err);}
-		res.render("index",{total_propiedades: total_propiedades});	
+		var articles = [
+			{	
+				title: "Qué es HTML",
+				url:"https://codigofacilito.com/articulos/43",
+				image:"http://codigofacilito.com/photo_articulos_store/43.jpg"
+			},
+			{	
+				title: "Selectores CSS",
+				url:"https://codigofacilito.com/articulos/42",
+				image:"http://codigofacilito.com/photo_articulos_store/42.jpg"
+			},
+			{	
+				title: "Commits - Administrar tu repositorio",
+				url:"https://codigofacilito.com/articulos/41",
+				image:"http://codigofacilito.com/photo_articulos_store/41.jpg"
+			},
+			{	
+				title: "Cómo crear una cuenta y un repositorio en Github",
+				url:"https://codigofacilito.com/articulos/39",
+				image:"http://codigofacilito.com/photo_articulos_store/39.jpg"
+			},
+			{	
+				title: "Qué es Git",
+				url:"https://codigofacilito.com/articulos/39",
+				image:"http://codigofacilito.com/photo_articulos_store/38.jpg"
+			},
+			{	
+				title: "Media Queries",
+				url:"https://codigofacilito.com/articulos/37",
+				image:"http://codigofacilito.com/photo_articulos_store/37.jpg"
+			}
+
+		];
+		res.render("index",{total_propiedades: total_propiedades});
+		
 	});
 	
 });
