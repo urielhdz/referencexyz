186959e707c051a34011078e9c63596a376bdc40
urielhdz <uriel.hdzc@gmail.com>
2015-08-28 15:58:37 -0500

    Mejoras en la vista del lenguaje y quitamos el menú hamburguesa

--- app.js
+++ app.js
@@ -110,10 +110,20 @@ router.route("/lenguajes/:id")
 		if(req.params.id === "new"){
 			res.render("languages/new");	
 		}else{
-
+			var cursos = [
+				{title: "Diseño Web Frontend", link:"https://codigofacilito.com/premium/frontend",
+					img_url: "http://codigofacilito.com/system/cursos/avatars/000/000/042/medium/42.png?1422556431"
+				},
+				{title: "CSS Básico a Avanzado", link:"https://codigofacilito.com/premium/css3",
+					img_url: "http://codigofacilito.com/system/cursos/avatars/000/000/041/medium/41.png?1422556455"
+				},
+				{title: "HTML5 Avanzado", link:"https://codigofacilito.com/premium/html5",
+					img_url: "http://codigofacilito.com/system/cursos/avatars/000/000/043/medium/43.png?1422556412"
+				}
+			];
 			Language.findById(req.params.id,function(err,language){
 				Property.find({language: language._id},function(err,propiedades){
-					res.render("languages/show",{language: language, propiedades: propiedades});
+					res.render("languages/show",{language: language, propiedades: propiedades,cursos:cursos});
 				});
 			});
 		}
