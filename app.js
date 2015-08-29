b12e40b42e15918f725d558fa3fcdc1a4fedd5c7
urielhdz <uriel.hdzc@gmail.com>
2015-08-29 11:42:35 -0500

    Env variables changed

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ var app = express();
 var connection_string = '127.0.0.1:27017/nodetest';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+  connection_string = process.env.MONGO_DB_USER + ":" +
+  process.env.MONGO_DB_PASSWORD + "@" +
+  process.env.MONGO_DB_HOST + ':' +
+  process.env.MONGO_DB_PORT + '/' +
+  process.env.MONGO_DB_DATABASE;
 }
 
 mongoose.connect("mongodb://"+connection_string);
