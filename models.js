f73362f4c044bface51d9d4e203ad7062fd5d160
urielhdz <uriel.hdzc@gmail.com>
2015-08-28 21:07:21 -0500

    Mejor mes meta tags

--- models.js
+++ models.js
@@ -24,6 +24,23 @@ property_schema.virtual("url").get(function(){
 	return this.slug || this._id;
 });
 
+property_schema.virtual("desc").get(function(){
+	var input = this.description;
+	allowed = "";
+	allowed = (((allowed || '') + '')
+    .toLowerCase()
+    .match(/<[a-z][a-z0-9]*>/g) || [])
+    .join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+
+  var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+      commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+
+  return input.replace(commentsAndPhpTags, '')
+      .replace(tags, function($0, $1) {
+          return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+      });
+});
+
 var Property = mongoose.model("Propiedad",property_schema);
 
 var user_schema = new Schema({
